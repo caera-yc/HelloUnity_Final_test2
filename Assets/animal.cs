6711616c69b948aa1c6577cc5c30080b1a7db8f6
caera-yc <38510526+caera-yc@users.noreply.github.com>
2018-05-31 18:29:26 +1200

    Initial commit

---
+++ Assets/animal.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+//Make sure you get rid of the namespace that is automatically generated here.
+class Animal
+{
+    //List all the columns of your table here in the same format as below.
+    public string AnimalType { get; set; }
+    public string ActivitiesType { get; set; }
+    
+}
